8491053eba35595161d61d4a566f400faad42e75
danielbeja10 <131400214+danielbeja10@users.noreply.github.com>
2025-09-11 19:15:18 +0300

    Update lbr_logger.c

--- lbr_logger.c
+++ lbr_logger.c
@@ -1,6 +1,9 @@
+#include <linux/kernel.h>
+#include <linux/types.h>
 #include <linux/errno.h>
 #include <linux/slab.h>
 #include <linux/minmax.h>
+#include <linux/printk.h>
 #include <asm/msr.h>
 #include "lbr_API.h"   /* lbr_limits definition and macros */
 #include "lbr_info.h"  /* lbr_get_support, lbr_get_limits */
@@ -29,8 +32,10 @@ int lbr_log_init(struct lbr_log *log)
     log->entries = kcalloc(lim.max_depth, sizeof(*log->entries), GFP_KERNEL); // create space for the entries of the TO and FROM.
     if (!log->entries) // check if *log has enough space.
         return -ENOMEM;
+      
 
     log->capacity = lim.max_depth; //the LBR can contain max depth number of branches.
+    pr_info("lbr: created array of %u lbr entries"\n, log->capacity);   
     log->count = 0; // how many branches we recorded.
     return 0;
 }
@@ -63,26 +68,18 @@ int lbr_log_capture(struct lbr_log *log)
         return rc;
 
     n = min(lim.current_depth, log->capacity); // we might have CPU capacity of 32 but we only want current depth of 16.
-
+    pr_info("capture %u LBR records\n", n);
     for (i = 0; i < n; i++) {
         u64 from = 0, to = 0;
         rdmsrl(IA32_LBR_FROM_BASE + i, from);  
         rdmsrl(IA32_LBR_TO_BASE + i, to);    
         log->entries[i].from = from;
         log->entries[i].to   = to;
+        pr_info("captured[%u]: from=%#llx to=%#llx\n",
+                i, (unsigned long long)from, (unsigned long long)to);
     }
 
     log->count = n;
     return 0;
 }
 
-void lbr_log_print(const struct lbr_log *log)
-{
-    u32 i;
-    if (!log || !log->entries)
-        return;
-
-    for (i = 0; i < log->count; i++) {
-        pr_info("lbr[%u]: %px -> %px\n", i,(void *)log->entries[i].from,(void *)log->entries[i].to); // print in a nice format the brancheds
-    }
-}
